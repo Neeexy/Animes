2811b68786d0973871ebf336add72c6bafe69636
Neeexy <nexy.2809@gmail.com>
2024-10-22 11:33:41 -0300

    a

--- Js/video-control.js
+++ Js/video-control.js
@@ -85,10 +85,7 @@ rewindButton.setAttribute('title', 'Rewind 10 seconds');
 
 // Adiciona o texto ao botão
 rewindButton.innerHTML = `<span class="rewind10s" aria-hidden="true">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-        <polygon points="11 19 2 12 11 5 11 19" />
-        <polygon points="22 19 13 12 22 5 22 19" />
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 19 2 12 11 5 11 19"></polygon><polygon points="22 19 13 12 22 5 22 19"></polygon></svg>
 </span>
 <span class="vjs-control-text" aria-live="polite">Rewind 10s</span>`;
 
