0d1d882cf3153b02d523a83411695a99c46b98b5
Neeexy <nexy.2809@gmail.com>
2024-10-24 16:24:30 -0300

    a

--- thumb-create.py
+++ thumb-create.py
@@ -1,33 +1,263 @@
-import os
-import subprocess
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="anime, BLEACH, One Piece, Naruto, Attack on Titan, Dragon Ball Z, My Hero Academia, Sword Art Online, Demon Slayer, Fullmetal Alchemist, Tokyo Ghoul, Hunter x Hunter, Death Note, Black Clover, Jujutsu Kaisen, Fairy Tail, Re:Zero, One Punch Man, Cowboy Bebop, Neon Genesis Evangelion, Code Geass, Steins;Gate, Mob Psycho 100, Yu Yu Hakusho, Rurouni Kenshin, Akame ga Kill, Vinland Saga, The Seven Deadly Sins, Gintama, Dr. Stone, Fate/Stay Night, Parasyte, Kuroko no Basket, Haikyuu, Toradora, Clannad, Your Lie in April, Elfen Lied, Bakuman, Inuyasha, Shingeki no Kyojin, Konosuba, Overlord, The Rising of the Shield Hero, Mushoku Tensei, Darling in the Franxx, Fire Force, Noragami, Erased, Angel Beats, Kill la Kill, Made in Abyss, Assassination Classroom, Zankyou no Terror, Blue Exorcist, Hellsing, Trigun, Psycho-Pass, Mirai Nikki, Boku Dake ga Inai Machi, FLCL, Detective Conan, K-On!, Ao no Exorcist, Spice and Wolf, Soul Eater, No Game No Life, Guilty Crown, Durarara!!, Log Horizon, Kaguya-sama: Love is War, The Promised Neverland, Bakemonogatari, Nisekoi, Highschool DxD, ReLife, Mahou Shoujo Madoka Magica, Oregairu, Shokugeki no Souma, Nanatsu no Taizai, Gurren Lagann, D.Gray-man, Air Gear, Great Teacher Onizuka, Ergo Proxy, Blood+, Highschool of the Dead, Vampire Knight, Fate/Zero, Claymore, Hyouka, Chihayafuru, Monogatari Series, Higurashi no Naku Koro ni, Another, Tokyo Revengers, Kaichou wa Maid-sama!, Sket Dance, Katekyo Hitman Reborn, Shaman King, Fruits Basket, Lovely★Complex">
 
-# Lista de episódios
-episodios = ['11']  # Por exemplo, episódios 1 a 5
-
-# Caminho base para os vídeos e miniaturas
-base_video_url = "https://cdn-s01.mywallpaper-4k-image.net/stream/d/dungeon-ni-deai-o-motomeru-no-wa-machigatteiru-darou-ka/"
-base_thumb_path = 'thumb-frames'
-
-# Função para converter segundos em formato MM:SS
-def format_time(seconds):
-    minutes = seconds // 60
-    seconds = seconds % 60
-    return f"{minutes:02}:{seconds:02}"
-
-for episodio in episodios:
-    # Criar diretório para cada episódio
-    episodio_dir = os.path.join(base_thumb_path, str(episodio))
-    os.makedirs(episodio_dir, exist_ok=True)
+    <meta property="og:description" content="Nexy Animes - Animes Online, Assistir Animes Online. Assista Animes Dublados e Legendados, ou Faça downloads. Animes em Full HD e HD.">
+    <meta property="og:title" content="Nexy Animes - Animes Br - Assistir Animes Online Grátis -">
+    <meta name="description" content="Nexy Animes - Animes Online, Assistir Animes Online. Assista Animes Dublados e Legendados, ou Faça downloads. Animes em Full HD e HD.">
+    <meta property="og:image:secure_url" content="https://i.ibb.co/T8C7xh7/Nexy-Banner.png">
+    <meta property="og:image" content="https://i.ibb.co/T8C7xh7/Nexy-Banner.png">
+    <meta name="twitter:image" content="https://i.ibb.co/T8C7xh7/Nexy-Banner.png">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://nexyanimes.netlify.app/">
+    <meta name="keywords" content="anime, BLEACH, dublado, legendado, assistir online, animes completos">
+    <meta name="author" content="Nexy Animes">
+    <meta charset="UTF-8">
+    <title>Nexy Animes - Início</title>
+    <link rel="shortcut icon" href="style/components/Nexy Animes logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/style-index.css">
+    <link rel="stylesheet" href="style/index-thumb.css">
+</head>
+<body>
+    <header>
+        <nav id="nav">
+            <div class="logo" id="logo">
+                <a href="https://nexyanimes.netlify.app" target="_self"><img src="style/components/Nexy Animes nav.png" alt="Nexy Animes logo"></a>
+            </div>
     
-    # URL do vídeo
-    video_url = f"{base_video_url}/{episodio}.mp4/index.m3u8"
+            <div class="search-box">
+                <button class="btn-search">
+                    <i class="fas fa-search"><ion-icon name="search-outline" class="search-ico"></ion-icon></i>
+                </button>
+                <input type="text" class="input-search" placeholder="Buscar Animes..">
+            </div>
+        </nav>
+        <div id="search-results" class="search-results"></div>
+    </header>
+<div id="page-content">
     
-    # Gerar miniaturas em tempos diferentes
-    for time in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264, 266, 268, 270, 272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296, 298, 300, 302, 304, 306, 308, 310, 312, 314, 316, 318, 320]:  # Exemplo de tempos: 10s, 30s, 60s, 90s, 120s
-        output_path = os.path.join(episodio_dir, f"thumbnail-{time}s.jpg")
-        formatted_time = format_time(time)  # Converte o tempo para MM:SS
-        command = [
-            "ffmpeg", "-i", video_url, "-ss", f"00:{formatted_time}.000",
-            "-vframes", "1", "-update", "1", output_path
-        ]
-        subprocess.run(command)
+        <main>
+            <h2 class="topic">Animes Em Lançamento</h2>
+            <article class="anime-dragger" id="a-g">
+
+            <div class="animes-head" id="animes-container">
+
+            </div>
+
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a  href="assistir-anime/Re-Zero-1/Re-Zero.html" target="_self">
+                            <img src="https://i.ibb.co/LSqnJc9/re-zero-kara-hajimeru-isekai-seikatsu.jpg" alt="Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka - poster" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Re:Zero Kara Hajimeru Isekai Seikatsu</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-anime/Re-Zero-1/Re-Zero.html" target="_self">Re:Zero</a></h3>
+                        <span class="date">2016</span>
+                    </div>
+                </div>
+
+
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a  href="https://nexyanimes.netlify.app/assistir-anime/dungeon%20ni%20deai/dungeon-ni-deai" target="_self">
+                            <img src="https://i.ibb.co/1q7tCDn/dungeon-ni-deai-poster-full.jpg" alt="Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka - poster" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Dungeon ni Deai</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="https://nexyanimes.netlify.app/assistir-anime/dungeon%20ni%20deai/dungeon-ni-deai" target="_self">Dungeon ni Deai</a></h3>
+                        <span class="date">2015</span>
+                    </div>
+                </div>
+
+
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a  href="assistir-anime/bleach-tybw/bleach-tybw.html" target="_self">
+                            <img src="https://static.anroll.net/images/animes/capas/bleach-sennen-kessen-hen-ketsubetsu-tan.jpg" alt="Bleach" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Bleach</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-anime/bleach-tybw/bleach-tybw.html" target="_self">Bleach - TYBW</a></h3>
+                        <span class="date">2022</span>
+                    </div>
+                </div>
+    
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a href="assistir-anime/Uzumaki/uzimaki.html" target="_self">
+                            <img src="https://i.ibb.co/wSdqZhS/uzumaki-grande.jpg" alt="Uzumaki" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Uzumaki</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-anime/Uzumaki/uzimaki.html" target="_self">Uzumaki</a></h3>
+                        <span class="date">2024</span>
+                    </div>
+                </div>
+    
+    
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a href="assistir-anime/Tsue to Tsurugi no Wistoria/Wistoria.html" target="_self">
+                            <img src="https://i.ibb.co/DQ1QCmh/wistoria-poster.jpg" alt="Tsue to Tsurugi no Wistoria" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Tsue to Tsurugi no Wistoria</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-anime/Tsue to Tsurugi no Wistoria/Wistoria.html" target="_self">Tsue to Tsurugi no Wistoria</a></h3>
+                        <span class="date">2024</span>
+                    </div>
+                </div>
+    
+    
+                <div class="animes-head">
+                    <div class="anime-card">
+                        <a href="assistir-anime/Blue Lock/Blue Lock.html" target="_self">
+                            <img src="https://i.ibb.co/Bym4D7k/Blue-Lock-Poster.jpg" alt="Blue Lock" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Blue Lock</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-anime/Blue Lock/Blue Lock.html" target="_self">Blue Lock</a></h3>
+                        <span class="date">2022</span>
+                    </div>
+                </div>
+    
+    
+    
+            </article>
+    
+            <div id="topic"><h2 class="topic" style="margin-top: 15px;">Últimos Episódios</h2></div>
+    
+            <section class="episodes-list" id="episodes-container">
+            </section>
+
+
+            <h2 class="topic">Filmes </h2>
+
+
+            <article class="anime-dragger" id="a-g">
+
+                <div class="animes-head">
+
+                    <div class="anime-card">
+                        <a  href="assistir-filme/Tonari-No-Totoro.html" target="_self">
+                            <img src="https://m.media-amazon.com/images/I/81RkM26bXwL._AC_SL1500_.jpg" alt="Tonari No Totoro" class="poster">
+    
+                        </a>
+                        <div class="card-content">
+                            <h2>Tonari No Totoro</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-filme/Tonari-No-Totoro.html" target="_self">Tonari No Totoro</a></h3>
+                        <span class="date">1988</span>
+                    </div>
+                </div>
+                    
+                    <div class="animes-head">
+                    <div class="anime-card">
+                        <a  href="assistir-filme/Dungeon-Ni-Deai-Orion-no-Ya.html" target="_self">
+                            <img src="https://cdn.myanimelist.net/images/anime/1239/96949.jpg" alt="Tonari No Totoro" class="poster">
+                            
+                        </a>
+                        <div class="card-content">
+                            <h2>Dungeon ni Deai Movie: Orion no Ya</h2>
+                            <div class="card-rating">
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star" class="star"></ion-icon>
+                                <ion-icon name="star-half" class="star"></ion-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="data-feature">
+                        <h3 class="anime-title"><a href="assistir-filme/Dungeon-Ni-Deai-Orion-no-Ya.html" target="_self">Dungeon ni Deai Movie: Orion no Ya</a></h3>
+                        <span class="date">2019</span>
+                    </div>
+                    </div>
+
+
+            </article>
+    
+        </main>
+    
+    </div>
+    
+    
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="Js/search.js"></script>
+    <script src="Js/dragger.js" ></script>
+    <script src="Js/episodes.js"></script>
+
+</body>
+
+</html>
